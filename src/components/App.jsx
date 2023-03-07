86a82350f3e4a62d1f92e923f2a98fe9aa3c7432
Maksym Lukianenko <maks.lukyan@gmail.com>
2023-03-07 15:02:23 +0200

    add LS

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -18,7 +18,14 @@ export class App extends Component {
       this.setState({ contacts: parsedContacts });
       return;
     }
-    this.setState({ contacts: [] });
+    this.setState({
+      contacts: [
+        { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+        { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+        { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+        { id: 'id-4', name: 'Annia Copeland', number: '227-91-26' },
+      ],
+    });
   }
 
   componentDidUpdate(prevProps, prevState) {
